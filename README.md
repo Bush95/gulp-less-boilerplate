9e655df775b8dc42435f0a5c7b3bce7f8abeffaa
Bush95 <adi139597@gmail.com>
2016-10-31 21:30:54 +0100

    Boilerplate commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # gulp-less-boilerplate
 Basic gulp configuration with livereload server and less compiler
+
+This boilerplate is running on gulp livereload server, so your browser needs LiveReload extention.
+
+### How to start
+
+Install all packages:
+```sh
+$ npm install
+```
+
+Run dev server:
+```sh
+$ gulp dev
+```
+or simply 
+```sh
+$ gulp
+```
+
+Server will track files for change and localhost page will immediately refresh.
+Server localport is set to 8000 by default.
+
+### Production
+
+To run production, type:
+
+```sh
+$ gulp production
+```
+
+### License
+
+[MIT License](https://github.com/Bush95/gulp-less-boilerplate/blob/master/LICENSE.md)
---
+++ gulpfile.js
@@ -0,0 +1,136 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    del = require('del'),
+    cleanCss = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    server = require('gulp-server-livereload'),
+    concat = require('gulp-concat'),
+    cleanJs = require('gulp-clean'),
+    uglify = require('gulp-uglify'),
+    purifyCss = require('gulp-purifycss'),
+    runSequence = require('run-sequence'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('build-clean', function () {
+    return del(['dist', 'temp']);
+});
+
+gulp.task('styles', function() {
+    return gulp.src('src/less/main.less')
+    .pipe(less({compress: false}))
+    .pipe(gulp.dest('temp/css'));
+});
+
+gulp.task('copyIndex', function() {
+    return gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copyFav', function() {
+    return gulp.src('src/favicon/*')
+    .pipe(gulp.dest('dist/favicon'));
+});
+
+gulp.task('copyImages', function() {
+    return gulp.src('src/images/*')
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('copyMinImages', function() {
+    return gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('concatBs', function () {
+    return gulp.src(['node_modules/bootstrap-grid/dist/grid.css', 'temp/css/main.css'])
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('temp/css'));
+});
+
+gulp.task('cleanJs', function () {
+    return gulp.src('temp/js/main.js')
+    .pipe(cleanJs())
+    .pipe(gulp.dest('temp/js'));
+});
+
+gulp.task('concatJs', function() {
+    return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'src/js/main.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('temp/js'));
+});
+
+gulp.task('minCss', function () {
+    return gulp.src('temp/css/main.css')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(purifyCss(['dist/index.html', 'temp/js/main.js']))
+    .pipe(cleanCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('minJs', function () {
+    return gulp.src('temp/js/main.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+
+gulp.task('devCss', function() {
+    return gulp.src('temp/css/main.css')
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('devJs', function () {
+    return gulp.src('temp/js/main.js')
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/index.html', ['copyIndex']);
+    gulp.watch('src/less/*', () => runSequence('styles', 'concatBs', 'devCss'));
+    gulp.watch('src/js/*.js', () => runSequence('concatJs', 'devJs'));
+    gulp.watch('src/images/*', ['copyImages']);
+});
+
+gulp.task('server', function() {
+    return gulp.src('dist')
+      .pipe(server({
+        livereload: true,
+        open: true
+      }))
+});
+
+gulp.task('dev', function () {
+  runSequence('build-clean',
+              'cleanJs',
+              'concatJs',
+              'devJs',
+              'copyIndex',
+              'copyImages',
+              'styles',
+              'concatBs',
+              'devCss',
+              'server',
+              'watch');
+});
+
+gulp.task('default', ['dev']);
+
+gulp.task('production', function (callback) {
+
+  runSequence('build-clean',
+              'copyIndex',
+              'copyFav',
+              'copyMinImages',
+              'styles',
+              'concatJs',
+              'cleanJs',
+              'minJs',
+              'concatBs',
+              'minCss',
+              callback);
+  }
+);
